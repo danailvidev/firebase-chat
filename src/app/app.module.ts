9790752277e5705dd81eb18981563c1a908f01bb
Danail Videv <danailvidev@gmail.com>
2019-07-02 16:09:27 +0300

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -10,14 +11,17 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +13,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  chat$: Observable<any>;
+  newMsg: string;
 
-  constructor() { }
+  constructor(
+    public cs: ChatService,
+    private route: ActivatedRoute,
+    public auth: AuthService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    const chatId = this.route.snapshot.paramMap.get('id');
+    const source = this.cs.get(chatId);
+    this.chat$ = this.cs.joinUsers(source);
+
   }
 
+  submit(chatId) {
+    this.cs.sendMessage(chatId, this.newMsg);
+    this.newMsg = '';
+  }
+
+  trackByCreated(i, msg) {
+    return msg.createdAt;
+  }
+
+  back() {
+    this.location.back();
+  }
 }
