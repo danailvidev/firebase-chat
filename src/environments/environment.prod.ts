bf2d3e010d9f1c6a97c0bf7ecf4a8e042b83eec9
Danail Videv <danailvidev@gmail.com>
2019-07-02 14:44:41 +0300

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyARnUW3B3HfAfSJoBR8UGAJ0iMMfeqhUnE',
+    authDomain: 'chatapp-9b186.firebaseapp.com',
+    databaseURL: 'https://chatapp-9b186.firebaseio.com',
+    projectId: 'chatapp-9b186',
+    storageBucket: 'chatapp-9b186.appspot.com',
+    messagingSenderId: '1006917067850',
+    appId: '1:1006917067850:web:b6385c9d22462bdf'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyARnUW3B3HfAfSJoBR8UGAJ0iMMfeqhUnE',
+    authDomain: 'chatapp-9b186.firebaseapp.com',
+    databaseURL: 'https://chatapp-9b186.firebaseio.com',
+    projectId: 'chatapp-9b186',
+    storageBucket: 'chatapp-9b186.appspot.com',
+    messagingSenderId: '1006917067850',
+    appId: '1:1006917067850:web:b6385c9d22462bdf'
+  }
 };
 
 /*
