43a86fd127d9733b88fdc2db7562072dfd48b2da
Danail Videv <danailvidev@gmail.com>
2019-07-02 16:08:40 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChatComponent } from './chat/chat.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
+  { path: '', component: WelcomeComponent },
   { path: 'chats/:id', component: ChatComponent }
 ];
 
